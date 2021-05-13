3405ddcad65645fd9458b194006b5f3d997d6d77
Vladimir Rusinov <vladimir.rusinov@gmail.com>
2021-05-13 22:21:37 +0100

    FLOW001 in Rust

---
+++ FLOW001/README.md
@@ -0,0 +1,19 @@
+# Add Two Numbers
+
+Problem Code: [FLOW001](https://www.codechef.com/problems/FLOW001)
+
+This is a tutorial program.
+
+## Problem Statement
+
+Shivam is the youngest programmer in the world, he is just 12 years old. Shivam is learning programming and today he is writing his first program.
+
+Program is very simple, given two integers A and B, write a program to add these two numbers.
+
+## Input
+
+The first line contains an integer T, the total number of test cases. Then follow T lines, each line contains two Integers A and B.
+
+## Output
+
+For each test case, add A and B and display it in a new line.
---
+++ FLOW001/src/main.rs
@@ -0,0 +1,33 @@
+// Add Two Numbers
+//
+// https://www.codechef.com/problems/FLOW001
+
+fn add(c: Vec<i32>) -> i32 {
+    return c.iter().sum();
+}
+
+fn main() {
+    use std::io::{self, BufRead};
+
+    let stdin = io::stdin();
+    let mut cin_iterator = stdin.lock().lines();
+
+    // Read number of cases.
+    // It is actually not used, we simply read until EOF later.
+    let buffer = cin_iterator.next().unwrap().unwrap();
+    let _: i32 = buffer.trim().parse().expect("input was not an integer");
+
+    // Read cases from stdin, line by line.
+    for line in cin_iterator {
+        // Shove them into vec of i32
+        let c = line
+            .unwrap()
+            .split_whitespace()
+            .map(|x| x.parse::<i32>())
+            .collect::<Result<Vec<i32>, _>>()
+            .unwrap();
+
+        let ret = add(c);
+        println!("{}", ret);
+    }
+}
