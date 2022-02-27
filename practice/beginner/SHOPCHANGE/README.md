431b0864902ad312ea864acb37ef1afb6a00ed9d
Vladimir Rusinov <vladimir.rusinov@gmail.com>
2022-02-27 19:49:28 +0000

    SHOPCHANGE problem

---
+++ practice/beginner/SHOPCHANGE/README.md
@@ -0,0 +1,7 @@
+# Shopping Change
+
+https://www.codechef.com/problems/SHOPCHANGE
+
+Chef went shopping and bought items worth X rupees (1≤X≤100). Unfortunately, Chef only has a single 100 rupees note.
+
+Since Chef is weak at maths, can you help Chef in calculating what money he should get back after paying 100 rupees for those items?
---
+++ practice/beginner/SHOPCHANGE/shopchange.go
@@ -0,0 +1,53 @@
+// Package main implements Shopping Change problem
+// https://www.codechef.com/problems/SHOPCHANGE
+package main
+
+import (
+  "bufio"
+  "os"
+  "strconv"
+  "strings"
+  "fmt"
+)
+
+const (
+	totalMoney = 100
+)
+
+func calculateChange(x int) int {
+	return totalMoney - x
+}
+
+func readLine(reader *bufio.Reader) (string, error) {
+    str, _, err := reader.ReadLine()
+    if err != nil {
+        return "", err
+    }
+
+    return strings.TrimRight(string(str), "\r\n"), nil
+}
+
+func readInt(reader *bufio.Reader) (int, error) {
+  s, err := readLine(reader)
+  if err != nil {
+    return 0, err
+  }
+  return strconv.Atoi(s)
+}
+
+func main() {
+  reader := bufio.NewReaderSize(os.Stdin, 1024 * 1024)
+
+  t, err := readInt(reader)
+  if err != nil {
+      panic(err)
+  }
+
+  for i := 0; i < t; i++ {
+    x, err := readInt(reader)
+    if err != nil {
+      panic(err)
+    }
+    fmt.Println(calculateChange(x))
+  }
+}
