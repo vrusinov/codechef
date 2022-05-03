26c4099479f5f5bfcb03554762425ebee4f2d20f
Vladimir Rusinov <vladimir.rusinov@gmail.com>
2022-05-03 22:00:21 +0100

    Make gometalinter optional

--- presubmit.sh
+++ presubmit.sh
@@ -5,7 +5,7 @@ set -e
 # Run go linters in all go directories
 for d in $(find . -name \*.go | sed -r 's|/[^/]+$||' |sort -u) ; do
     golint $d
-    gometalinter $d
+    which gometalinter && gometalinter $d
 done
 
 pytype .
