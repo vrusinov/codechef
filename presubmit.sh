6032efef829ae7c0c99a17c4af53530f8653eaec
Vladimir Rusinov <vladimir.rusinov@gmail.com>
2022-06-05 12:17:36 +0100

    Disable gometalinter

--- presubmit.sh
+++ presubmit.sh
@@ -5,7 +5,6 @@ set -e
 # Run go linters in all go directories
 for d in $(find . -name \*.go | sed -r 's|/[^/]+$||' |sort -u) ; do
     golint $d
-    which gometalinter && gometalinter $d
 done
 
 pytype .
