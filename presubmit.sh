a52e9b3fef48e9f25c1bdd5dc857f2b83e0e54d6
Vladimir Rusinov <vladimir.rusinov@gmail.com>
2022-09-13 21:44:26 +0100

    Disable golint

--- presubmit.sh
+++ presubmit.sh
@@ -4,7 +4,6 @@ set -e
 
 # Run go linters in all go directories
 for d in $(find . -name \*.go | sed -r 's|/[^/]+$||' |sort -u) ; do
-    golint $d
     pushd $d
     golangci-lint run
     popd    
