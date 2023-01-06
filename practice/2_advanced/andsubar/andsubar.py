766a8466e1a166e510ecfa2fe0da35440fb2e46e
Vladimir Rusinov <vladimir.rusinov@gmail.com>
2023-01-06 15:47:43 +0000

    Formatting adjustments

--- practice/2_advanced/andsubar/andsubar.py
+++ practice/2_advanced/andsubar/andsubar.py
@@ -7,17 +7,19 @@
 
 import math
 
+
 def and_subarr(n):
     if n == 1:
         return 1
     # Need highest power of two and all following digits.
-    highest_p2 = 2**int(math.log(n, 2))
+    highest_p2 = 2 ** int(math.log(n, 2))
     remainder = n - highest_p2
     # Here we either go from highest power of 2 to the end of array, or take the
     # length of the sequence following previous highest power of two.
-    return max(remainder+1, highest_p2//2)
+    return max(remainder + 1, highest_p2 // 2)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     t = int(input())
     for _ in range(t):
         n = int(input())
