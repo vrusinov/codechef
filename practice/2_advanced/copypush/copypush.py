0dc285067c3e8f4c13361dde9cac8d99c8adcdf4
Vladimir Rusinov <vladimir.rusinov@gmail.com>
2023-01-07 17:33:09 +0000

    flake8 fixes

--- practice/2_advanced/copypush/copypush.py
+++ practice/2_advanced/copypush/copypush.py
@@ -16,13 +16,13 @@ def copypush(line: str, prev_op: str = ""):
     Try splitting or cutting characters from line to see if we can end up
     in empty string. Keep track of previous actions.
     """
-    l = len(line)
-    if l == 0:
+    line_len = len(line)
+    if line_len == 0:
         return "yes"
     # First, try to see if we can split the string into two equals.
-    if l % 2 == 0:
-        if line[: l // 2] == line[l // 2 :]:
-            if copypush(line[l // 2 :], prev_op=COPY) == "yes":
+    if line_len % 2 == 0:
+        if line[:line_len // 2] == line[line_len // 2:]:
+            if copypush(line[line_len // 2:], prev_op=COPY) == "yes":
                 return "yes"
     # We tried splitting and it didn't work. Let's see if we can append.
     if prev_op == PUSH_BACK:
