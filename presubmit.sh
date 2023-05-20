24bdde8bac0c1c028a52f427baa04e9da884905d
Vladimir Rusinov <vladimir.rusinov@gmail.com>
2023-05-20 20:16:44 +0100

    Remove golint-ci from presubmit.sh

--- presubmit.sh
+++ presubmit.sh
@@ -7,13 +7,6 @@
 
 set -e
 
-# Run go linters in all go directories
-for d in $(find . -name \*.go | sed -r 's|/[^/]+$||' |sort -u) ; do
-    pushd $d
-    golangci-lint run
-    popd
-done
-
 pytype .
 
 reuse lint
