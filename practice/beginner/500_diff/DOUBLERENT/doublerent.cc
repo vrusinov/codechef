15dfced5db8f53d0592e6d934d3f199e2ba35bb9
Vladimir Rusinov <vladimir.rusinov@gmail.com>
2024-10-13 10:12:35 +0100

    DOUBLERENT

---
+++ practice/beginner/500_diff/DOUBLERENT/doublerent.cc
@@ -0,0 +1,18 @@
+/* SPDX-FileCopyrightText: 2024 Vladimir Rusinov <vladimir.rusinov@gmail.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/*
+ * https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/DOUBLERENT
+ */
+
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+int main() {
+  int x;
+  cin >> x;
+  cout << x * 2;
+}
